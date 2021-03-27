1862f011c82bcb40fb5824c6ada1ab97b64cfeed
Francine Guimarães <francinesguimaraes@outlook.com>
2021-03-27 01:29:29 -0300

    Fix problem in MinimalTodo.parse

--- minimal_todo/lib/minimal_todo.ex
+++ minimal_todo/lib/minimal_todo.ex
@@ -2,27 +2,24 @@ defmodule MinimalTodo do
   @moduledoc """
   Documentation for `MinimalTodo`.
   """
+  def start do
+    filename = IO.gets("Hi there! Please, inform your .csv file to be loaded: ")
+    |> String.trim()
+    read(filename)
+  end
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> MinimalTodo.hello()
-      :world
-
-  """
   def read(filename) do
     case File.read(filename) do
-      {:ok, body} -> body
-      {:error, _reason} -> IO.puts ~s(Could not open file "#{filename}\n")
-
+      {:ok, body}       -> body
+      {:error, reason}  -> IO.puts ~s(Could not open file "#{filename}\n")
+                           IO.puts ~s("#{:file.format_error reason}"\n)
+                           start()
     end
   end
 
   def parse(body) do
-    [header | lines] = String.split(body, ~r{(\r\n|\r\n)})
-    titles = tl String.split(header, ",")
+    [header | lines] = String.split(body, ~r{(\r\n|\r|\n)})
+    titles = String.split(header, ",")
     parse_lines(lines, titles)
   end
 
