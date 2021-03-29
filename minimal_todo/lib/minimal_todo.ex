f35b068faa029f06c9d51ced6c6a52d1d63fb98a
Francine Guimarães <francinesguimaraes@outlook.com>
2021-03-29 02:49:11 -0300

    Add MinimalTodo.delete_todos

--- minimal_todo/lib/minimal_todo.ex
+++ minimal_todo/lib/minimal_todo.ex
@@ -3,14 +3,32 @@ defmodule MinimalTodo do
   Documentation for `MinimalTodo`.
   """
   def start do
-    filename = IO.gets("Hi there! Please, inform your .csv file to be loaded: ")
+    IO.gets("Hi there! Please, inform your .csv file to be loaded: ")
     |> String.trim()
     |> read()
     |> parse()
     |> get_command()
   end
 
-  def get_command(data), do: data
+  def get_command(data) do
+    menu = """
+    Choose an option:
+    [R]ead todos    [A]dd todos    [D]elete todos
+    [L]oad file     [S]ave file    [Q]uit
+    """
+
+    command =IO.gets(menu)
+    |> String.trim()
+    |> String.downcase()
+
+    case command do
+      "r" -> show_todos(data)
+      "d" -> delete_todos(data)
+      "q" -> IO.puts("Goodbye! :)")
+       _  -> IO.puts("\nOops! Invalid option\n")
+             get_command(data)
+    end
+  end
 
   def read(filename) do
     case File.read(filename) do
@@ -49,4 +67,21 @@ defmodule MinimalTodo do
       get_command(data)
     end
   end
+
+  def delete_todos(data) do
+    show_todos(data, false)
+
+    todo = IO.gets("Witch todo would you like to delete? ")
+    |> String.trim()
+    if(Map.has_key?(data, todo)) do
+      new_map = Map.drop(data, [todo])
+      IO.puts(~s(#{todo} has been deleted\n))
+      get_command(new_map)
+    else
+      IO.puts(~s(There is no todo named #{todo}))
+      show_todos(data, false)
+      get_command(data)
+
+  end
+end
 end
