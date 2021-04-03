d1608127e12227126084c53ca9fbc56f66f276c4
Francine Guimarães <francinesguimaraes@outlook.com>
2021-04-03 16:30:31 -0300

    Add deal, save and load functions

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -7,4 +7,23 @@ defmodule Cards do
         "#{value} of #{suit}"
     end
   end
+
+  def deal(deck, hand_size) do
+    Enum.shuffle(deck)
+    |> Enum.split(hand_size)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck) # Converts 'deck' into a binary content
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    {status, content} = File.read(filename)
+
+    case status do
+    :ok -> :erlang.binary_to_term(content)
+    :error -> "Sorry, I couldn't find the file you inform :("
+    end
+  end
 end
