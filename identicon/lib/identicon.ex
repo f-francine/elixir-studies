b78854e843bbf391a8eed531b94e32715adbb22e
Francine Guimarães <francinesguimaraes@outlook.com>
2021-04-05 03:18:54 -0300

    Feat: pick a color from a hash

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -1,13 +1,22 @@
 defmodule Identicon do
+
+  alias Identicon.Image
   def main(input) do
     input
     |> hash_input()
+    |> pick_color()
+  end
+
+  def pick_color(%Image{hex: [red, green, blue | _tail]} = image) do
+    %Image{image | color: {red, green, blue}}
+
+    [red, green, blue]
   end
 
   def hash_input(input) do
     hex = :crypto.hash(:md5, input)
     |> :binary.bin_to_list()
 
-    %Identicon.Image{hex: hex}
+    %Image{hex: hex}
   end
 end
