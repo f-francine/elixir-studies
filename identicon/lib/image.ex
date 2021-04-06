522bc4feabab29134c399c5474eb399abdb8d222
Francine Guimarães <francinesguimaraes@outlook.com>
2021-04-06 03:15:04 -0300

    Feat: generate an identicon image

--- identicon/lib/image.ex
+++ identicon/lib/image.ex
@@ -1,3 +1,3 @@
 defmodule Identicon.Image do
-  defstruct hex: nil, color: nil, grid: nil # Hex (hexacidemal) property that will stars as nil
+  defstruct hex: nil, color: nil, grid: nil, pixel_map: nil # Hex (hexacidemal) property that will stars as nil
 end
